5e8b6515380f52f39d3be632d933fa61b066ca41
Kazu Komoto <kazuhiro.komoto@students.makeschool.com>
2015-11-17 07:42:53 -0800

    Modified

--- MakeSchoolNotes/ViewControllers/NewNoteViewController.swift
+++ MakeSchoolNotes/ViewControllers/NewNoteViewController.swift
@@ -38,6 +38,7 @@ class NewNoteViewController: UIViewController {
             currentNote = Note()
             let noteViewController = segue.destinationViewController as! NoteDisplayViewController
             noteViewController.note = currentNote
+            noteViewController.edit = true
         }
 /*
         currentNote = Note()
--- MakeSchoolNotes/ViewControllers/NoteDisplayViewController.swift
+++ MakeSchoolNotes/ViewControllers/NoteDisplayViewController.swift
@@ -8,23 +8,53 @@
 import Foundation
 import UIKit
 import RealmSwift
+import ConvenienceKit
+
 
 class NoteDisplayViewController: UIViewController {
     
 
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var contentTextView: UITextView!
+    @IBOutlet weak var deleteButton: UIBarButtonItem!
+    @IBOutlet weak var toolbarBottomSpace: NSLayoutConstraint!
+    
+    var keyboardNotificationHandler: KeyboardNotificationHandler?
+    
+    var edit: Bool = false
     
     var note: Note? {
         didSet {
             displayNote(note)
         }
     }
-
+    
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
         displayNote(note)
+        
+        titleTextField.returnKeyType = .Next
+        titleTextField.delegate = self
+        
+        keyboardNotificationHandler = KeyboardNotificationHandler()
+        
+        keyboardNotificationHandler!.keyboardWillBeHiddenHandler = { (height: CGFloat) in UIView.animateWithDuration(0.3){
+                self.toolbarBottomSpace.constant = 0
+                self.view.layoutIfNeeded()
+            }
+        }
+        
+        keyboardNotificationHandler!.keyboardWillBeShownHandler = { (height: CGFloat) in UIView.animateWithDuration(0.3){
+            //FIXME
+                self.toolbarBottomSpace.constant = -height
+                self.view.layoutIfNeeded()
+            }
+        }
+        
+        if edit {
+            deleteButton.enabled = false
+        }
     }
     
     //MARK: Business Logic
@@ -32,6 +62,10 @@ class NoteDisplayViewController: UIViewController {
         if let note = note, titleTextField = titleTextField, contentTextView = contentTextView {
             titleTextField.text = note.title
             contentTextView.text = note.content
+            
+            if note.title.characters.count == 0 && note.content.characters.count == 0 {
+                titleTextField.becomeFirstResponder()
+            }
         }
     }
     
@@ -41,7 +75,7 @@ class NoteDisplayViewController: UIViewController {
         saveNote()
     }
     
-    func saveNote() {
+    func saveNote(){
         if let note = note {
             do {
                 let realm = try Realm()
@@ -56,5 +90,17 @@ class NoteDisplayViewController: UIViewController {
                 print("handle error")
             }
             }
+    }
+    
+}
+
+extension NoteDisplayViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if (textField == titleTextField) {
+            contentTextView.returnKeyType = .Done
+            contentTextView.becomeFirstResponder()
         }
+        
+        return false
+    }
 }
